ace09af31c4bf49e8890bbf72c18976f3e39cf28
noa <noasagi88@gmail.com>
2026-01-18 15:27:38 +0200

    חיבור בין הדף הרשמה לטבלת נתונים

--- hadasAravWebSite/Register.aspx.cs
+++ hadasAravWebSite/Register.aspx.cs
@@ -1,19 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
-public partial class _Default : System.Web.UI.Page
+public partial class harshama : Page
 {
-    public string name, welcome;
+    public string st = "";
+
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (IsPostBack)
+        if (Page.IsPostBack)
         {
-            name =Request.Form["name"];
-            welcome = "Welcome to the website," + name + "!";
+            string fn = Request.Form["name"];
+            string age = Request.Form["age"];
+
+            string sqlInsert =
+                "INSERT INTO tUsers VALUES (" +
+                "N'" + fn + "'," +
+                age +
+                ")";
+
+            MyAdoHelper.DoQuery("MyDB.mdf", sqlInsert);
+
+            st = "נרשמת בהצלחה!";
         }
     }
+}
+
 }
